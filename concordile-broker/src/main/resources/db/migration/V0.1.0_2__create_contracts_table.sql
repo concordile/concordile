b3a5126fb11dcd423ed5bda8a1d0040aed66a118
Grig Alex <alexengrigdev@gmail.com>
2026-05-01 21:04:37 +0300

    feat(broker): add contracts persistence skeleton
    Signed-off-by: Grig Alex <alexengrigdev@gmail.com>

---
+++ concordile-broker/src/main/resources/db/migration/V0.1.0_2__create_contracts_table.sql
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025-present The Concordile Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+create table if not exists contracts
+(
+    id          uuid primary key not null default gen_random_uuid(),
+    version     bigint           not null default 0,
+    created_at  timestamptz      not null default now(),
+    modified_at timestamptz      not null default now(),
+    deleted_at  timestamptz      null,
+    producer_id uuid             not null references applications (id),
+    consumer_id uuid             null references applications (id),
+    path        text             not null,
+    name        text             not null
+);
